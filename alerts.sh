fb6350fc50831b0f9bd1e74e362448ffba19c82e
Tom Paulus <tom@tompaulus.com>
2016-11-04 16:48:37 -0700

    Updated Module Scripts to reflect Vault Changes.

--- alerts.sh
+++ alerts.sh
@@ -4,7 +4,7 @@ export VAULT_ADDR="https://vault.example.com:8200" # Path to your Vault
 export VAULT_ROLE="thisismyfancyID" # Your Vault AppRole ID
 export VAULT_SECRET="thisismyfancysecret" # Your Vault AppRole SECRET
 
-java -jar followup.jar > /dev/null 2>&1
+java -jar alerts.jar > /dev/null 2>&1
 # STOUT can be ignored as log information is saved to log file.
 
 # Environment variables don't need to by unset, because they don't exist outside of this session
