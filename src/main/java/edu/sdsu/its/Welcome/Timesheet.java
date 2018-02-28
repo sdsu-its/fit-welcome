94fa8343f7c93b6d830ac4418f2192655e4d5b53
Tom Paulus <tom@tompaulus.com>
2018-02-27 23:19:44 -0800

    Added DOW to Timesheet and fixed Rounding Issue

--- src/main/java/edu/sdsu/its/Welcome/Timesheet.java
+++ src/main/java/edu/sdsu/its/Welcome/Timesheet.java
@@ -2,6 +2,7 @@ package edu.sdsu.its.Welcome;
 
 import com.opencsv.CSVWriter;
 import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 
 import java.io.File;
@@ -14,9 +15,11 @@ import java.util.TreeMap;
  * Generate Timesheet based on ClockIO {@link Clock.ClockIO} entries.
  *
  * @author Tom Paulus
- *         Created on 12/21/15.
+ * Created on 12/21/15.
  */
 public class Timesheet {
+    private static final String[] DAYS_OF_THE_WEEK = {"Su", "M", "T", "W", "Th", "F", "Sa"};
+
     /**
      * Generate a Timesheet with the total number of hours/minutes worked on a given day.
      * Days where no hours were worked will not be included.
@@ -26,13 +29,13 @@ public class Timesheet {
      * @return {@link File} Generated TimeSheet CVS
      */
     public static File make(Clock.ClockIO[] entries, final String fileName) {
-        Map<String, Double> days = new TreeMap<>(); // Day:Minutes Worked
+        Map<DateTime, Double> days = new TreeMap<>(); // Day:Minutes Worked
         for (Clock.ClockIO entry : entries) {
             if (entry.inTime == null || entry.outTime == null) {
                 continue;
             }
 
-            final String inTime = entry.inTime.toString(DateTimeFormat.forPattern("yyyy-MM-dd"));
+            final DateTime inTime = entry.inTime;
             if (days.containsKey(inTime)) {
                 days.put(inTime, days.get(inTime) + entry.duration.getStandardSeconds() / 60d);
             } else {
@@ -47,14 +50,17 @@ public class Timesheet {
             Logger.getLogger(Timesheet.class).info(String.format("Saving Timesheet into %s.csv", fileName));
 
             CSVWriter writer = new CSVWriter(new FileWriter(file));
-            writer.writeNext(new String[]{"Day", "Hours", "Minutes", "Hours Fractional"}); // Header Row
+            writer.writeNext(new String[]{"Day", "Day of Week", "Hours", "Minutes", "Hours Fractional"}); // Header Row
 
-            for (String day : days.keySet()) {
-                final int hours = (int) (days.get(day) / 60);
-                final int min = (int) (days.get(day) % 60);
-                final String hours_fractional = String.format("%.1f", days.get(day) / 60);
+            for (DateTime day : days.keySet()) {
+                final String date = day.toString(DateTimeFormat.forPattern("yyyy-MM-dd"));
+                final String dow = DAYS_OF_THE_WEEK[day.dayOfWeek().get()];
+                final String hours = Integer.toString((int) (days.get(day) / 60));
+                final String min = Integer.toString((int) (days.get(day) % 60));
+                final String hours_fractional = String.format("%.1f", days.get(day) / 60 + 0.05);
+                // .05 is added to ensure that the number of hours is always rounded up.
 
-                writer.writeNext(new String[]{day, Integer.toString(hours), Integer.toString(min), hours_fractional});
+                writer.writeNext(new String[]{date, dow, hours, min, hours_fractional});
             }
             writer.close();
         } catch (IOException e) {
